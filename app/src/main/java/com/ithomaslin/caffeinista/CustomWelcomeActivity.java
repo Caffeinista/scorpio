798032094a760ccc5cf5facb1d5a60ce9002e49a
Thomas Lin <ithomaslin@gmail.com>
2016-08-03 07:26:30 +0800

    Adding SignIn activity

---
+++ app/src/main/java/com/ithomaslin/caffeinista/CustomWelcomeActivity.java
@@ -0,0 +1,12 @@
+package com.ithomaslin.caffeinista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomWelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
