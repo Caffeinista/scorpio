8e0ec247f38d04f41b41c3cee33c89c6b8b68a53
Thomas Lin <ithomaslin@gmail.com>
2016-08-05 01:54:30 +0800

    Adding new layout and library

--- app/src/main/java/com/ithomaslin/caffeinista/CustomWelcomeActivity.java
+++ app/src/main/java/com/ithomaslin/caffeinista/CustomWelcomeActivity.java
@@ -13,11 +13,17 @@ public class CustomWelcomeActivity extends WelcomeActivity {
     protected WelcomeScreenConfiguration configuration() {
         return new WelcomeScreenBuilder(this)
                 .theme(R.style.CustomWelcomeScreenTheme)
-                .titlePage(R.drawable.welcom_icon, getString(R.string.welcome_title_page), R.color.primary, true)
-                .parallaxPage(R.layout.welcome_screen_1, getString(R.string.welcome_one_title), getString(R.string.welcome_one_description), R.color.primary, 0.2f, 2f)
-                .parallaxPage(R.layout.welcome_screen_2, getString(R.string.welcome_two_title), getString(R.string.welcome_two_description), R.color.primary, 0.2f, 2f)
-                .swipeToDismiss(false)
+                .parallaxPage(R.layout.welcome_main, getString(R.string.welcome_title_page),
+                        getString(R.string.welcome_title_description), R.color.primary, 0.2f, 2f)
+                .parallaxPage(R.layout.welcome_screen_1, getString(R.string.welcome_one_title),
+                        getString(R.string.welcome_one_description), R.color.primary, 0.2f, 2f)
+                .parallaxPage(R.layout.welcome_screen_2, getString(R.string.welcome_two_title),
+                        getString(R.string.welcome_two_description), R.color.primary, 0.2f, 2f)
+                .parallaxPage(R.layout.welcome_screen_3, getString(R.string.welcome_three_title),
+                        getString(R.string.welcome_three_description), R.color.primary, 0.2f, 2f)
                 .useCustomDoneButton(true)
+                .backButtonSkips(false)
+                .swipeToDismiss(false)
                 .build();
     }
 }
